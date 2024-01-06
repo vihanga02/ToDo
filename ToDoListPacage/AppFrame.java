83dde50ffb4f538c74a7698b57a01c821937c409
vihanga02 <muthumalavdw.22@uom.lk>
2024-01-06 13:29:22 +0530

    Title Done

--- ToDoListPacage/AppFrame.java
+++ ToDoListPacage/AppFrame.java
@@ -1,10 +1,15 @@
 package ToDoListPacage;
 
 import javax.swing.JFrame;
+import java.awt.*;
+
 public class AppFrame extends JFrame{
+    TitleBar title = new TitleBar();
     public AppFrame(){
         this.setSize(400,700);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
+
+        this.add(this.title, BorderLayout.NORTH);
     }
 }
--- ToDoListPacage/TitleBar.java
+++ ToDoListPacage/TitleBar.java
@@ -1,6 +1,19 @@
 package ToDoListPacage;
 
+import javax.imageio.plugins.tiff.TIFFTagSet;
 import javax.swing.*;
+import java.awt.*;
 
-public class TitleBar extends JFrame {
+public class TitleBar extends JPanel {
+
+    JLabel TitleText = new JLabel("ToDO List");
+    public TitleBar(){
+        this.setPreferredSize(new Dimension(400,80));
+        this.setBackground(new Color(150,150,150));
+
+        TitleText.setPreferredSize(new Dimension( 200,80));
+        TitleText.setFont(new Font("sans-serif", Font.BOLD, 30));
+        TitleText.setHorizontalAlignment(JLabel.CENTER);
+        this.add(this.TitleText);
+    }
 }
